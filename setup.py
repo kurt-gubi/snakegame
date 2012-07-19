3288b71967a6baf2ae77faeb244bc2332484e8d4
Peter Ward <peteraward@gmail.com>
2012-07-19 20:10:40 +1000

    Add packaging.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='SnakeGame',
+    version='1.0',
+    description='The game of Snake, for beginner AI bot writers.',
+    author='Peter Ward',
+    author_email='peteraward@gmail.com',
+    packages=['snakegame'],
+    scripts=[],
+)
