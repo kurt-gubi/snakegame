985d2c0e98e1705f266ad2014976549841350a1d
Peter Ward <peteraward@gmail.com>
2014-06-30 08:49:54 +1000

    fix stupid package things

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name='SnakeGame',
@@ -6,13 +6,13 @@ setup(
     description='The game of Snake, for beginner AI bot writers.',
     author='Peter Ward',
     author_email='peteraward@gmail.com',
-    packages=['snakegame'],
+    packages=find_packages(),
     zip_safe=False,
     install_requires=[
         'six',
     ],
     package_data={
-        'snakegame': 'images/*.png',
+        'snakegame': ['images/*.png'],
     },
     entry_points={
         'console_scripts': [
